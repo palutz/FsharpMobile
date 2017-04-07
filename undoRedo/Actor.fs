87c542d5ece4e30dd7401d1e0d8af22158182433
palutz <stefano.paluello@gmail.com>
2017-04-07 22:56:33 +0200

    adding actor to the app (code not working.. need to refactor and move type in a core file/lib)

---
+++ undoRedo/Actor.fs
@@ -0,0 +1,22 @@
+﻿namespace undoRedo
+
+type UndoRedoMsg =
+  | Undo
+  | Redo
+  | Add of ColorInfo
+
+module Actor =
+  open System.Collections.ObjectModel
+
+  let undoRedoActor = 
+    MailboxProcessor<string>.Start(fun inbox ->
+      let rec inner (undo: ObservableCollection<ColorInfo>) (redo: ObservableCollection<ColorInfo>) =
+        async {
+          let! msg = iinbox.Receive()
+          match msg with 
+          | Add m -> undo.Insert(0, m)
+          | Redo -> ()
+          | Undo -> ()
+          return! inner undo redo 
+        }
+      loop undo redo 
