ba0f95745d5098cdd430ab3a3893f4d52a74eb38
palutz <stefano.paluello@gmail.com>
2017-04-07 21:46:43 +0200

    undo and redo lists, observable collections, push on the undo stack

--- undoRedo/UndoRedo.fs
+++ undoRedo/UndoRedo.fs
@@ -3,6 +3,15 @@
 open Xamarin.Forms
 open Xamarin.Forms.Xaml
 open System
+open System.Collections.ObjectModel
+
+
+
+type ColorInfo = {
+  Name : string
+  Color: Color
+}
+
 
 type UndoRedoPage() =
   inherit ContentPage()
@@ -13,22 +22,32 @@ type UndoRedoPage() =
   do base.LoadFromXaml(typeof<UndoRedoPage>) |> ignore
 
   let helloLabel = base.FindByName<Label>("helloLabel")
+  let redoList = base.FindByName<ListView>("redoLV")
+  let undoList = base.FindByName<ListView>("undoLV")
 
+  let undoL = new ObservableCollection<ColorInfo>()
+  let redoL = new ObservableCollection<ColorInfo>()
   do 
+    // init items
     helloLabel.BackgroundColor <- Color.Red
-  
+    redoList.ItemsSource <- redoL
+    undoList.ItemsSource <- undoL
   
   member __.OnButtonRedClicked (sender:Object) (args:EventArgs) =
     helloLabel.BackgroundColor <- Color.Red
-
+    undoL.Insert(0, { Name="Red"; Color=Color.Red })
+  
   member __.OnButtonFuchsiaClicked (sender:Object) (args:EventArgs) =
     helloLabel.BackgroundColor <- Color.Fuchsia
+    undoL.Insert(0, { Name="Fuchsia"; Color=Color.Fuchsia })
 
   member __.OnButtonGreenClicked (sender:Object) (args:EventArgs) =
     helloLabel.BackgroundColor <- Color.Green
+    undoL.Insert(0, { Name="Green"; Color=Color.Green })
 
   member __.OnButtonYellowClicked (sender:Object) (args:EventArgs) =
     helloLabel.BackgroundColor <- Color.Yellow
+    undoL.Insert(0, { Name="Yellow"; Color=Color.Yellow })
   
 
 type App() =
